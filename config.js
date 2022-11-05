01b8c41f535c9bb045c2313b651bb5bd6c7c60a4
MubiynDev <mubiynsafiyulloh@icloud.com>
2022-11-05 16:10:23 +0500

    fixed bugs 5

--- config.js
+++ config.js
@@ -2,7 +2,10 @@ require("dotenv").config()
 const { env } = process
 
 module.exports = {
+    API_TOKEN: env.API_TOKEN,
     TOKEN: env.TOKEN,
     PORT: env.PORT,
-    SERVER_URL: env.SERVER_URL   
+    SERVER_URL: env.SERVER_URL,
+    CHANNEL_FOR_LOG: env.CHANNEL_FOR_LOG,
+    FETCH_URL: env.FETCH_URL
 }
--- src/bot.js
+++ src/bot.js
@@ -1,51 +1,27 @@
 const { default: fetch } = require('node-fetch');
-const express = require("express")
-const app = express()
 const TelegramBot = require("node-telegram-bot-api");
-const { TOKEN, PORT, SERVER_URL } = require("../config");
-const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
-const URI = `/webhook/${TOKEN}`
-const webhookURL = `${SERVER_URL}${URI}`
+const { TOKEN, PORT, SERVER_URL, CHANNEL_FOR_LOG, FETCH_URL, API_TOKEN } = require("../config");
 
-const setWebhook = async () => {
-    try {
-        await fetch(`${TELEGRAM_API}/setWebhook?url=${webhookURL}&drop_pending_updates=true`)
-    } catch (error) {
-        return error
+const bot = new TelegramBot(TOKEN, {
+    webHook: {
+        port: PORT,
+        autoOpen: false
     }
-}
+});
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
-app.post(URI, async (req, res) => {
-    bot.processUpdate(req.body)
-    res.status(200).send("ok")
-})
-app.listen(PORT, async () => {
-    try {
-        console.log(`server running at ${PORT}`)
-        await setWebhook()
-    } catch(e) {
-        console.log(e.message)
-    }
-})
-
-///////////////////////////////////////////////////////////////////////////////////
+bot.openWebHook()
+bot.setWebHook(`${SERVER_URL}/bot${TOKEN}`);
 
 
-const bot = new TelegramBot(TOKEN);
-
-
-
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 bot.on("message", async message => {
     const chatId = message.from.id;
     const text = message.text;
     const name = message.from.first_name
     
-    
-    await bot.forwardMessage("@MsXbotlogger", chatId, message.message_id)
+
+    await bot.forwardMessage(CHANNEL_FOR_LOG, chatId, message.message_id)
 
     
     if(text === "/start") {
@@ -66,11 +42,11 @@ bot.on("message", async message => {
             input: { img: link  }
            }
         
-        let result = await fetch("https://api.replicate.com/v1/predictions", {
+        let result = await fetch(FETCH_URL, {
             method: 'post',
             headers: {
                 'Content-Type' : 'application/json',
-                'Authorization' : 'Token 667312eaae60d0ed2921b5194473e8cacf36ef84'
+                'Authorization' : API_TOKEN
                      },
             body: JSON.stringify(data)
         
@@ -82,7 +58,7 @@ bot.on("message", async message => {
                 method: 'GET',
                 headers: { 
                     'Content-Type'  : 'application/json',
-                    'Authorization' : 'Token 667312eaae60d0ed2921b5194473e8cacf36ef84' 
+                    'Authorization' : API_TOKEN 
                     }
             })
            image = await image.json()
